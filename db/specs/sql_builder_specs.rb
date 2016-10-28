3efc42abfddcc291c50068c12ef1f579be18403d
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-28 16:28:44 +0100

    adding all_where and all_where_id methods, supporting methods and tests

--- db/specs/sql_builder_specs.rb
+++ db/specs/sql_builder_specs.rb
@@ -1,5 +1,6 @@
 require('minitest/autorun')
 require('minitest/rg')
+require('date')
 
 require_relative('../sql_builder')
 
@@ -9,6 +10,57 @@ class SqlBuilderTest < MiniTest::Test
     expected = "SELECT * FROM customers"
     actual = SqlBuilder.select_all_sql("customers")
     assert_equal(expected, actual)
+  end
+
+  def test_all_where_sql__multiple_conditions()
+    expected = "SELECT * FROM customers WHERE first_name = 'Matthew' AND last_name = 'Jeorrett'"
+    conditions = {
+      first_name: "Matthew",
+      last_name: "Jeorrett"
+    }
+    actual = SqlBuilder.all_where_sql("customers", conditions)
+    assert_equal(expected, actual)
+  end
+
+  def test_all_where_sql__single_condition()
+    expected = "SELECT * FROM customers WHERE id = 4"
+    actual = SqlBuilder.all_where_sql("customers", { id: 4 })
+    assert_equal(expected, actual)
+  end
+
+  def test_select_where_id_sql()
+    expected = "SELECT * FROM customers WHERE id = 7"
+    actual = SqlBuilder.all_where_id_sql( 'customers', 7 )
+    assert_equal( expected, actual )
+  end
+
+  def test_value_to_sql__string()
+    assert_equal("'Matthew'", SqlBuilder.value_to_sql("Matthew"))
+  end
+
+  def test_value_to_sql__fixnum()
+    assert_equal("14", SqlBuilder.value_to_sql(14))
+  end
+
+  def test_value_to_sql__float()
+    assert_equal("14.33", SqlBuilder.value_to_sql(14.33))
+  end
+
+  def test_value_to_sql__trueclass()
+    assert_equal( "TRUE", SqlBuilder.value_to_sql( true ) )
+  end
+
+  def test_value_to_sql__falseclass()
+    assert_equal( "FALSE", SqlBuilder.value_to_sql( false ) )
+  end
+
+  def test_value_to_sql__nilclass()
+    assert_equal( "NULL", SqlBuilder.value_to_sql( nil ))
+  end
+
+  def test_value_to_sql__dateclass()
+    date = Date.parse("2016-10-10")
+    assert_equal( "'2016-10-10'", SqlBuilder.value_to_sql( date ) )
   end
 
 end
--- db/sql_builder.rb
+++ db/sql_builder.rb
@@ -1,7 +1,69 @@
+require('date')
+
 class SqlBuilder
 
   def self.select_all_sql( table_name )
     return "SELECT * FROM #{table_name}"
+  end
+
+  def self.all_where_sql( table_name, conditions_hash )
+    select_statement = self.select_all_sql(table_name)
+    where_clause = self.where_clause( conditions_hash )
+
+    return "#{select_statement} #{where_clause}"
+  end
+
+  def self.all_where_id_sql( table_name, id )
+    conditions_hash = { id: id }
+    sql = self.all_where_sql( table_name, conditions_hash )
+    return sql
+  end
+
+  def self.where_clause( conditions_hash )
+    conditions_array = []
+
+    for column, value in conditions_hash
+      value_sql = self.value_to_sql(value)
+      sql = "#{column} = #{value_sql}"
+      conditions_array.push(sql)
+    end
+
+    conditions_sql = conditions_array.join(" AND ")
+
+    return "WHERE #{conditions_sql}"
+  end
+
+  def self.value_to_sql( value )
+    value_class = value.class().to_s()
+
+    case value_class
+
+    when 'Fixnum'
+      sql = "#{value}"
+
+    when 'Float'
+      sql = "#{value}"
+
+    when 'String'
+      sql = "'#{value.gsub("'", "''")}'"
+
+    when 'TrueClass'
+      sql = "TRUE"
+
+    when 'FalseClass'
+      sql = "FALSE"
+
+    when 'NilClass'
+      sql = "NULL"
+
+    when 'Date'
+      sql = "'#{value}'"
+
+    else
+      raise(TypeError, "Un-supported data type class: #{value_class}.")
+    end
+
+    return sql
   end
 
 end
