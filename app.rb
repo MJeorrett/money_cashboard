db55e29b155544735c8538636b5cd82ae688b37f
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-28 15:35:25 +0100

    all routes laid out

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ controllers = {}
 
 controllers['jedis'] = Controller.new( jedis_table )
 
+# INDEX
 get '/:table' do
 
   table = params[:table]
@@ -27,5 +28,59 @@ get '/:table' do
     erb_file = :'index'
   end
 
+  erb(erb_file)
+end
+
+# NEW
+get '/:table/new' do
+  table = params[:table]
+  @method = 'GET'
+  @route = "/#{table}/new"
+  erb_file = :'partials/page_not_found'
+  erb(erb_file)
+end
+
+# CREATE
+post '/:table' do
+  table = params[:table]
+  @method = 'POST'
+  @route = "/#{table}"
+  erb_file = :'partials/page_not_found'
+  erb(erb_file)
+end
+
+# SHOW
+get '/:table/:id' do
+  table = params[:table]
+  @method = 'GET'
+  @route = "/#{table}/:id"
+  erb_file = :'partials/page_not_found'
+  erb(erb_file)
+end
+
+# EDIT
+get '/:table/:id/edit' do
+  table = params[:table]
+  @method = 'GET'
+  @route = "/#{table}/:id/edit"
+  erb_file = :'partials/page_not_found'
+  erb(erb_file)
+end
+
+# UPDATE
+put '/:table/:id' do
+  table = params[:table]
+  @method = 'put'
+  @route = "/#{table}/:id"
+  erb_file = :'partials/page_not_found'
+  erb(erb_file)
+end
+
+# DESTROY
+delete '/:table/:id' do
+  table = params[:table]
+  @method = 'DELETE'
+  @route = "/#{table}/:id"
+  erb_file = :'partials/page_not_found'
   erb(erb_file)
 end
