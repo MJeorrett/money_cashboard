24b42bb84202f2ec31848cde1c0b749ac627dd40
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-28 16:11:22 +0100

    making 'page not found' handling dryer

--- app.rb
+++ app.rb
@@ -11,76 +11,51 @@ starwars_db = Database.new( 'star_wars' )
 jedis_table = starwars_db.table( 'jedis' )
 controllers = {}
 
-controllers['jedis'] = Controller.new( jedis_table )
+controllers['/jedis'] = Controller.new( jedis_table )
+
+# BEFORE
+before do
+  @path = request.path_info
+  @controller = controllers[@path]
+
+  if @controller.nil? && @path != '/page_not_found'
+    @method = request.request_method
+    redirect to('/page_not_found')
+  end
+
+end
+
+# PAGE NOT FOUND
+get '/page_not_found' do
+  erb(:'partials/page_not_found')
+end
 
 # INDEX
 get '/:table' do
-
-  table = params[:table]
-  controller = controllers[table]
-
-  if controller.nil?
-    @method = 'GET'
-    @route = "/#{table}"
-    erb_file = :'partials/page_not_found'
-  else
-    @data = controller.index_data()
-    erb_file = :'index'
-  end
-
-  erb(erb_file)
+  @data = @controller.index_data()
+  erb(:index)
 end
 
 # NEW
 get '/:table/new' do
-  table = params[:table]
-  @method = 'GET'
-  @route = "/#{table}/new"
-  erb_file = :'partials/page_not_found'
-  erb(erb_file)
 end
 
 # CREATE
 post '/:table' do
-  table = params[:table]
-  @method = 'POST'
-  @route = "/#{table}"
-  erb_file = :'partials/page_not_found'
-  erb(erb_file)
 end
 
 # SHOW
 get '/:table/:id' do
-  table = params[:table]
-  @method = 'GET'
-  @route = "/#{table}/:id"
-  erb_file = :'partials/page_not_found'
-  erb(erb_file)
 end
 
 # EDIT
 get '/:table/:id/edit' do
-  table = params[:table]
-  @method = 'GET'
-  @route = "/#{table}/:id/edit"
-  erb_file = :'partials/page_not_found'
-  erb(erb_file)
 end
 
 # UPDATE
 put '/:table/:id' do
-  table = params[:table]
-  @method = 'put'
-  @route = "/#{table}/:id"
-  erb_file = :'partials/page_not_found'
-  erb(erb_file)
 end
 
 # DESTROY
 delete '/:table/:id' do
-  table = params[:table]
-  @method = 'DELETE'
-  @route = "/#{table}/:id"
-  erb_file = :'partials/page_not_found'
-  erb(erb_file)
 end
